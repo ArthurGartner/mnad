985153e432207a683be183f4524d3bd5c6d54172
ArthurGartner <Arthur.Gartner@gmail.com>
2023-09-11 03:49:59 +0000

    Build: (a450cec) actions update

--- client/src/assets/index.js
+++
@@ -1,5 +0,0 @@
-import logoCoconut from "./logo_coconut.svg";
-import CoconutSmile from "./coconut_smile.svg";
-import BlankCoconut from "./coconut_dynamic.svg";
-
-export { logoCoconut, CoconutSmile, BlankCoconut };
--- client/src/components/DrinkName.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DrinkName = () => {
-  return <div>DrinkName</div>;
-};
-
-export default DrinkName;
